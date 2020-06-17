044198a325548f213260f7e76ed028c5310a612d
Dileepa <dileepa.rajapaksa@rhipe.com>
2020-06-17 15:43:55 +0800

    Add project files.

---
+++ ATKDemoDockerHub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ATKDemoDockerHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
